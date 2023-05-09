9f28cd4a1200c779e3ab9392ddaa9c814c28ed13
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-09 19:39:05 +0200

    10_71 completed

--- 10/71/main.c
+++ 10/71/main.c
@@ -11,19 +11,19 @@ void scanns(FILE *fp)
 {
     int ns[ROW_MAX][ROW_WIDTH];
     int i = 0;
-    while ((fscanf(fp, "%d\t%d\t%d\t%d\t", &ns[0][0], &ns[0][1], &ns[0][2],
-                   &ns[0][3])) == 4)
+    while ((fscanf(fp, "%d\t%d\t%d\t%d\t", &ns[i][0], &ns[i][1], &ns[i][2],
+                   &ns[i][3])) == 4)
         i++;
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
+    if (argc != 2) {
         printf("Invalid command line args - exiting...\n");
         return 1;
     }
 
-    char *infname = argv[1], *s = argv[2];
+    char *infname = argv[1];
     FILE *ifp = fopen(infname, "r");
     if (!ifp) {
         printf("Cant open '%s' - exiting...\n", infname);
