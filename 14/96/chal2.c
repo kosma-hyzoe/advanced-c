e5d3c67a6669341fd1e1c6b9114394ad3a0c056f
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-12 11:54:54 +0200

    13 and 14 complete

---
+++ 14/96/chal2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#define SUM(x, y) (x+y)
+
+int main()
+{
+    printf("%d\n", SUM(3, 5));
+}
---
+++ 14/96/chal3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define SQUARE(x) (x*x)
+#define CUBE(x) (x*x*x)
+
+int main()
+{
+    printf("%d\n", CUBE(3));
+}
