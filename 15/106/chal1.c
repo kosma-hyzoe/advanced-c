af1d280e55f6dcdcded40f644bc52c61dd5814d8
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-15 10:01:09 +0200

    15_106 misc improvements and corrections

--- 15/106/chal1.c
+++ 15/106/chal1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* `gcc chal1.c @flags` */
+/* `gcc chal1.c @flags1` */
 int main()
 {
     printf("\"Now I am become Death, the destroyer of worlds.\"\n");
--- 15/106/chal2.c
+++ 15/106/chal2.c
@@ -5,25 +5,19 @@
 #define DEBUG 0
 #endif
 
-#if DEBUG > 0
- #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
- __FILE__, __LINE__, __func__, ##args)
-#else
- #define DEBUG_PRINT(fmt, args...)
-#endif
+#define DBG_PRINT(lvl, fmt, args...) ((DEBUG >= lvl) ? \
+                      fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
+                      __FILE__, __LINE__, __func__, ##args) \
+                      : (void)0)
+
 
 int sum(int x, int y, int z)
 {
     char c = 2;
 
     int *a = NULL;
-    if (DEBUG > 1) {
-        DEBUG_PRINT("Params:\n");
-        fprintf(stderr, " x=%d\n", x);
-        fprintf(stderr, " y=%d\n", y);
-        fprintf(stderr, " z=%d\n", z);
-        fprintf(stderr, " a=%ld\n", (long)a);
-    }
+    if (DEBUG > 1)
+        DBG_PRINT(1, "\nParams:\n\tx=%d\n\ty=%d\n\tz=%d\n\ta=%ld\n", x, y, z, (long)a);
     *a = 5;
 
     fprintf(stderr, "*a=%d\n", *a);
@@ -33,34 +27,24 @@ int sum(int x, int y, int z)
 
 int main(int argc, char *argv[])
 {
-    DEBUG_PRINT("Debugging is enabled.\n");
-    DEBUG_PRINT("Debug level: %d\n", (int)DEBUG);
+    DBG_PRINT(1, "Debugging is enabled.\n");
+    DBG_PRINT(1,"Debug level: %d\n", (int)DEBUG);
     int i, j, k;
     int result;
 
-    #if DEBUG > 0
-     fprintf(stderr, "Number of parameters = %d\n", argc);
-    #endif
-
+    if (DEBUG > 0)
+        fprintf(stderr, "Number of parameters = %d\n", argc);
     if (argc == 1) {
         printf("Please specify three numbers as parameters.\n");
         exit(1);
     }
-
     i = atoi(argv[1]);
-
     fprintf(stderr, "i=%d\n", i);
-
     j = atoi(argv[2]);
-
     fprintf(stderr, "j=%d\n", j);
-
     k = atoi(argv[3]);
-
     fprintf(stderr, "k=%d\n", k);
-
     result = sum(i, j, 12) + sum(j, k, 19) + sum(i, k, 13);
-
     printf("%d\n", result);
 
     return 0;
