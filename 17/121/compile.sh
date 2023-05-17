ecf504a30368895154299394a94145d2596b23ae
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-17 12:02:37 +0200

    17_121 complete

--- 17/121/compile.sh
+++ 17/121/compile.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-gcc -shared -o libstrfun.so -fPIC src/strfun.c
-gcc -o run -Iincl -L. -lstrfun src/main.c
+# compile the shared object
+gcc -shared -o lib/libstrfun.so -fPIC src/strfun.c
+# compile the object file for main
+gcc -o obj/main.o -I ./incl -c src/main.c
+# link the library and compile the executable
+
+gcc -o run obj/main.o -L./lib -lstrfun # on windows: `-llibstrfun.dll`
+# `export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH` if -L is not used
--- 17/121/src/main.c
+++ 17/121/src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "strfun.h"
+#include "../incl/strfun.h"
 
 int main()
 {
