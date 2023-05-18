3b8708829bfa2d6e1f23153f9f88b02a3a82753c
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-18 08:59:38 +0200

    automated quick commit

---
+++ chptrs/18/131/chal2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define ARRAY_SIZE 20
+
+
+/* with random vals */
+void fillarray(double ar[], int n)
+{
+
+}
+
+void showarray(double ar[], int n)
+
+/* sorts array of doubles from lowest to highest */
+main()
+{
+
+}
---
+++ chptrs/18/131/chal3.c
@@ -0,0 +1,2 @@
+/* use time and ctime to print the current time */
+/* handle errors using fprintf and exit function */
