90a4c7e5176bbce7c627fb91340b5a4882ca5c99
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-19 16:24:50 +0200

    automated quick commit

---
+++ chptrs/19/136/README.md
@@ -0,0 +1,6 @@
+```c
+pos tmp  head  tail
+  1   2    12    34
+  2   3   123     4
+  3   4  1234   NULL
+```
--- chptrs/19/136/main.c
+++ chptrs/19/136/main.c
@@ -1,95 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct node {
     int val;
     struct node *next;
 } node_t;
 
-// TODO zero or one arguments?
-node_t *ndcreate()
+node_t *ndcreate(int val)
 {
-    node_t nd = malloc(sizeof(node_t));
+    node_t *nd = malloc(sizeof(node_t));
     if (!nd)
-        return 0;
-	nd.next = NULL;
-    return &node_t;
+        exit(1);
+    nd->next = NULL;
+    nd->val = val;
+    return nd;
 }
-
-void ndinsertbeg(node_t *nd, int val)
+void ndprint(node_t *nd)
 {
-    node_t *tail = ndcreate();
-
-    tail->val = val;
-    tail->next = nd;
-	nd = tail;
+    node_t *tmp = nd;
+    while (tmp != NULL) {
+        printf("%d->", tmp->val);
+        tmp = tmp->next;
+    }
+    printf("NULL\n");
 }
-
-void *ndinsertend(node_t *nd, int val)
+void ndinsbeg(node_t **nd, int val)
 {
-	node_t *head = ndcreate();
-	head->val = val;
-	nd->next = head;
+    node_t *tmp = *nd;
+    node_t *head = ndcreate(val);
+    head->next = tmp;
+    *nd = head;
 }
-void *ndinsertat(node *nd, int val, int pos)
+void ndinsend(node_t **nd, int val)
 {
-	int i = 0;
-	node_t *tmp = nd;
-	while (i != pos) {
-		if (tmp->next == NULL)
-			exit(1);
-		tmp = nd->next;
-		i++;
-	}
+    node_t *tmp = *nd;
+    while (tmp->next != NULL) {
+        tmp = tmp->next;
+    }
+    tmp->next = ndcreate(val);
 }
-node_t *nddelat();
-node_t *ndsearch();
-node_t *ndupdate();
-void nddisplay();
+int ndinsat(node_t **nd, int pos, int val)
+{
+    if (pos < 0) {
+        return EXIT_FAILURE;
+    } else if (pos == 0) {
+        ndinsbeg(nd, val);
+        return EXIT_SUCCESS;
+    }
+
+    node_t *tmp = *nd;
+    for (int i = 1; i < pos; i++) {
+        tmp = tmp->next;
+        if (!tmp)
+            return EXIT_FAILURE;
+    }
+    node_t *insert = ndcreate(val);
+    insert->next = tmp->next;
+    tmp->next = insert;
+    return EXIT_SUCCESS;
+}
+int nddelat(node_t **nd, int pos)
+{
+    if (pos < 0) {
+        return EXIT_FAILURE;
+    } else if (pos == 0) {
+        *nd = (*nd)->next;
+        return EXIT_SUCCESS;
+    }
+
+    node_t *tmp = *nd;
+    for (int i = 1; i < pos; i++) {
+        tmp = tmp->next;
+        if (!tmp)
+            return EXIT_FAILURE;
+    }
+    node_t *ndtodel = tmp->next;
+    tmp->next = ndtodel->next;
+    free(ndtodel);
+    return EXIT_SUCCESS;
+}
+int ndsearch(const node_t *nd, int val)
+{
+    int pos = 0;
+    while (nd != NULL) {
+        if (nd->val == val) {
+            return pos;
+        }
+        nd = nd->next;
+        pos++;
+    }
+    return -1; /* value not found */
+}
+int ndupdate(node_t **nd, int pos, int val)
+{
+    node_t *tmp = *nd;
+    for (int i = 0; i < pos; i++) {
+        tmp = tmp->next;
+        if (!tmp)
+            return EXIT_FAILURE;
+    }
+    tmp->val = val;
+    return EXIT_SUCCESS;
+}
 
 int main()
-{
-    node_t llist = ndcreate();
-    int choice = '\0';
+    node_t node = 
+    printf("Enter the value(s) for a new linked list and press 'q' when done: ")
+    while (1)
+    {
+        int ch = '\0';
+        printf("\n0. Create new node")
+        printf("\n1. Insert node at beginning");
+        printf("\n2. Insert node at end");
+        printf("\n3. Insert node at a specific position");
+        printf("\n4. Delete Node from any Position");
+        printf("\n5. Update Node Value");
+        printf("\n6. Search Element in the linked list");
+        printf("\n7. Display List");
+        printf("\n8. Exit\n");
+        printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
 
-    while (true) {
-        printf("1: Insert node at beginning");
-        printf("2: Insert node at end");
-        printf("3: Insert node at a specific position");
-        printf("4: Delete Node from any Position");
-        printf("5: Update Node Value");
-        printf("6: Search Element in the linked list");
-        printf("7: Display List");
-        printf("8: Exit");
-        scanf("%d", &choice);
-
-        switch (choice) {
-        case 1:
-            ndinsertbeg();
-            break;
-        case 2:
-            ndinsertend();
-            break;
-        case 3:
-            ndinsertat();
-            break;
-        case 4:
-            nddelat();
-            break;
-        case 5:
-            ndupdate();
-            break;
-        case 6:
-            ndsearch();
-            break;
-        case 7:
-            nddisplay();
-            break;
-        case 8:
-            printf("\n...Exiting...\n");
-            return 0;
-        default:
-            printf("\n...Invalid Choice...\n");
-            break;
-        }
+        switch (ch)
+        {
+		case 1:
+		    insertNodeAtTheBeginning();
+		    break;
+		case 2:
+		    insertNodeAtTheEnd();
+		    break;
+		case 3:
+		    insertNodeAtPosition();
+		    break;
+		case 4:
+		    deletePosition();
+		    break;
+		case 5:
+		    updateValue();
+		    break;
+		case 6:
+		    search();
+		    break;
+		case 7:
+		    display();
+		    break;
+		case 8:
+		    printf("\n...Exiting...\n");
+		    return 0;
+		default:
+		    printf("\n...Invalid Choice...\n");
+		    break;
+		}
     }
     return 0;
-}
+ }
+
 }
