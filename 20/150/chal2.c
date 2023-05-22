cefdf7ceb91bb9c83524341196d560225bcceda8
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-22 22:11:07 +0200

    automated quick commit

--- 20/150/chal2.c
+++ 20/150/chal2.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <sys/types.h>
-
+#include <unistd.h>
 
 int main()
 {
-    pid_t pid1 = fork();
-    pid_t pid2 = fork(); /* child1 will create a grandchild, child3 */
+    printf("Parent process  : PID %d\tPPID %d\n", getpid(), getppid());
 
-    if (pid1 > 0 && pid2 > 0) {
-        printf("Parent process  : PID %d\tPPID %d\n", getpid(), getppid());
-    } else {
-        int cc = (pid1 == 0 && pid2 > 0) ? 1 : (pid1 > 0 && pid2 == 0) ? 2 : 3;
-        printf("Child  process %d: PID %d\tPPID %d\n", cc, getpid(), getppid());
+    for (int i = 1; i < 4; i++) {
+        pid_t pid = fork();
+        if (pid > 0)
+            break;
+
+        printf("Child  process %d: PID %d\tPPID %d\n", i, getpid(), getppid());
     }
-    return 0;
 }
